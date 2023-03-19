dc3fcf896d0e37208095ce4e6c61b914b2ad8848
Yuzu <yuzu@kirameki.cafe>
2023-03-19 12:45:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const ip =
+        req.headers["x-forwarded-for"].split(", ")[0] ||
+        req.socket.remoteAddress;
 
     let data = "";
-    data = `IP: ${ip}\n`;
+    data = `IP: ${ip}<br/>`;
 
     for (let key in req.headers) {
         data += `${key}: ${req.headers[key]}`;
