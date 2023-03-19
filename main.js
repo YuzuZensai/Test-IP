956fb6cb623f63dfd417ebfff8ba4c0dd4fef4e5
Yuzu <yuzu@kirameki.cafe>
2023-03-19 11:24:18 +0700

    Send header back to client

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const app = express();
 
 app.get("/", (req, res) => {
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    res.send(ip);
+
+    let data = "";
+    data = `IP Address: ${ip}`;
+
+    for (let key in req.headers) {
+        data += `${key}: ${req.headers[key]}`;
+        data += "<br/>";
+    }
+    res.send(data);
     res.end();
 });
 
